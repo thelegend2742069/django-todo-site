bb58729dacdc5c7243225f1be610b0ae5cd8bace
Pranav Shirole <blab8650@gmail.com>
2023-09-10 03:02:39 +0530

    added update task functionality

--- tasks/views.py
+++ tasks/views.py
@@ -18,3 +18,17 @@ def index(request):
 
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TasksForm(instance=task)
+    context = {'form':form}
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+
+
+    return render(request, 'tasks/update_task.html', context)
