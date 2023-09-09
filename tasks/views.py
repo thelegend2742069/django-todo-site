3dcd6d06cada4f1e150ff4c653d04fe149813559
Pranav Shirole <blab8650@gmail.com>
2023-09-10 01:54:53 +0530

    added create new task form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    form = TasksForm
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+    context = {'tasks':tasks, 'form':form}
+    return render(request, 'tasks/list.html', context)
